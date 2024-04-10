2b094b6ba7dd604015c3af9cd6e339ff533c5207
Tanyi Győző <tanyi.gyozo@gmail.com>
2024-04-10 16:05:24 +0200

    Add modules/write/variables.tf

---
+++ modules/write/variables.tf
@@ -0,0 +1,34 @@
+variable "answer_1" {
+  type    = string
+  default = ""
+}
+
+variable "answer_2" {
+  type    = string
+  default = ""
+}
+
+variable "answer_3" {
+  type    = string
+  default = ""
+}
+
+variable "answer_4" {
+  type    = string
+  default = ""
+}
+
+variable "answer_5" {
+  type    = string
+  default = ""
+}
+
+variable "answer_6" {
+  type    = string
+  default = ""
+}
+
+variable "answer_7" {
+  type    = string
+  default = ""
+}
